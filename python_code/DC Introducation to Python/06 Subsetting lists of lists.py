55fbadeca24591caaf28e8a7d2e7c0a98c42bd74
ClaudRL <144999003+ClaudRL@users.noreply.github.com>
2024-09-23 23:30:42 +0200

    6

---
+++ python_code/DC Introducation to Python/06 Subsetting lists of lists.py
@@ -0,0 +1,11 @@
+house = [["hallway", 11.25],
+         ["kitchen", 18.0],
+         ["living room", 20.0],
+         ["bedroom", 10.75],
+         ["bathroom", 9.50]]
+
+# Subset the house list
+print(house[0])
+print(house[0][0])
+print(house[0][1])
+print(house[0][0][2])
