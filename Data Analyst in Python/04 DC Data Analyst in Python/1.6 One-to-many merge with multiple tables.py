03dbd417bd36c7cdb2f4d84e478d6c5e7f217c54
ClaudRL <144999003+ClaudRL@users.noreply.github.com>
2024-10-13 15:41:42 +0200

    Update 1.6 One-to-many merge with multiple tables.py

--- Data Analyst in Python/04 DC Data Analyst in Python/1.6 One-to-many merge with multiple tables.py
+++ Data Analyst in Python/04 DC Data Analyst in Python/1.6 One-to-many merge with multiple tables.py
@@ -10,4 +10,12 @@ land_cen_lic = land_use.merge(census, on = 'ward')\
                 .merge(licenses, on = 'ward',suffixes = ('_cen', '_lic'))
 
 # 2. Group land_cen_lic by ward, pop_2010 (the population in 2010), and vacant, then count the number of accounts. Save the results to pop_vac_lic.
+# Merge land_use and census and merge result with licenses including suffixes
+land_cen_lic = land_use.merge(census, on='ward') \
+                    .merge(licenses, on='ward', suffixes=('_cen','_lic'))
 
+# Group by ward, pop_2010, and vacant, then count the # of accounts
+pop_vac_lic = land_cen_lic.groupby(['ward','pop_2010','vacant'],
+                                   as_index=False).agg({'account':'count'})
+
+# 3. Sort pop_vac_lic by vacant, account, andpop_2010 in descending, ascending, and ascending order respectively. Save it as sorted_pop_vac_lic.
