b91849d69f01d63facbbbcc5bfecfb662f1822ad
ClaudRL <144999003+ClaudRL@users.noreply.github.com>
2024-10-22 23:55:12 +0200

    Create 2.4 Data back-ups.py

---
+++ Data Analyst in Python/05 Inroduction to Statistics in Python/2.4 Data back-ups.py
@@ -0,0 +1,5 @@
+# Exercise
+# Data back-ups
+# The sales software used at your company is set to automatically back itself up, but no one knows exactly what time the back-ups happen. It is known, however, that back-ups happen exactly every 30 minutes. Amir comes back from sales meetings at random times to update the data on the client he just met with. He wants to know how long he'll have to wait for his newly-entered data to get backed up. Use your new knowledge of continuous uniform distributions to model this situation and answer Amir's questions.
+
+# 1. To model how long Amir will wait for a back-up using a continuous uniform distribution, save his lowest possible wait time as min_time and his longest possible wait time as max_time. Remember that back-ups happen every 30 minutes.
