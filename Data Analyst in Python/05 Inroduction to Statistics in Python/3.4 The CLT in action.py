f1f0266e7f38ec4c5832d339d0552ecd041618d2
ClaudRL <144999003+ClaudRL@users.noreply.github.com>
2024-10-27 23:55:22 +0100

    Create 3.4 The CLT in action.py

---
+++ Data Analyst in Python/05 Inroduction to Statistics in Python/3.4 The CLT in action.py
@@ -0,0 +1,6 @@
+# The CLT in action
+# The central limit theorem states that a sampling distribution of a sample statistic approaches the normal distribution as you take more samples, no matter the original distribution being sampled from.
+
+# In this exercise, you'll focus on the sample mean and see the central limit theorem in action while examining the num_users column of amir_deals more closely, which contains the number of people who intend to use the product Amir is selling.
+
+# pandas as pd, numpy as np, and matplotlib.pyplot as plt are loaded and amir_deals is available.
